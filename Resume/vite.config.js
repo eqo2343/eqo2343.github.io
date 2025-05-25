5c5def9b8a66589524cdc61e74ba5c446f85785a
eqo234 <theedguy2@gmail.com>
2025-05-25 17:19:05 -0400

    updated vite config

--- Resume/vite.config.js
+++ Resume/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import images from 'rollup-plugin-image-assets'
 
 // https://vite.dev/config/
 export default defineConfig({
+  entry: 'src/index.js',
+  des: 'dist/bundle.js',
   plugins: [
     react(),
-  tailwindcss(),
+    tailwindcss(),
+    images({
+        output: 'build/images'
+    })
   ],
   server: {
     port: 3000
