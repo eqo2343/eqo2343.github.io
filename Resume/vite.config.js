98ce5eb5f129605fea66aed68b8c7db86b9d7536
eqo234 <theedguy2@gmail.com>
2025-05-25 17:27:51 -0400

    externalized images

--- Resume/vite.config.js
+++ Resume/vite.config.js
@@ -19,7 +19,13 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['axios']
+      external: ['axios', 
+        '/home/eqo234/resume-site/resume-site/Resume/dist/assets/hardlees1-7garVGrS.png',
+        '/home/eqo234/resume-site/resume-site/Resume/dist/assets/hardlees2-DuQ5IO7i.png',
+        '/home/eqo234/resume-site/resume-site/Resume/dist/assets/me2-Cw1j7ivo.jpg',
+        '/home/eqo234/resume-site/resume-site/Resume/dist/assets/snake-DNAFteSf.png',
+        '/home/eqo234/resume-site/resume-site/Resume/dist/assets/states-DGJHrd0q.png'
+      ]
     }
   }
 
